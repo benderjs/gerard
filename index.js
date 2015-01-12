4d1b933bf8765409c9cc3ae55e724135c048c311
Grzegorz Pabian <greg.pabian@gmail.com>
2015-01-12 16:48:23 +0100

    Support for a pattern as the input directory (WIP)

--- index.js
+++ index.js
@@ -11,27 +11,242 @@ var minimatch = require( 'minimatch' ),
 
 /**
  * Reads directory recursively
- * @param  {String}   dir                     Directory to be read
- * @param  {Object}   [options]               Configuration options
- * @param  {String}   [options.ignore]        Ignore file pattern, supports globstar matching
- * @param  {Boolean}  [options.stats]         Return file objects containing file statistics
- * @param  {Boolean}  [options.stopOnErrors]  Stop reading the directory on first error
- * @param  {Function} callback                Function called when done or error occured
+ * @param  {String}        dir                     Directory to be read
+ * @param  {Object}        [options]               Configuration options
+ * @param  {String|RegExp} [options.ignore]        Ignore file pattern, supports globstar matching
+ * @param  {Boolean}       [options.stats]         Return file objects containing file statistics
+ * @param  {Boolean}       [options.stopOnErrors]  Stop reading the directory on first error
+ * @param  {Function}      callback                Function called when done or error occured
  */
 function gerard( dir, options, callback ) {
-	var results = [],
-		count = 0;
-
+	// invalid argument count
 	if ( arguments.length < 2 ) {
 		return callback( new Error( 'Invalid number arguments for Gerard specified.' ) );
 	}
 
+	// only callback function was given
 	if ( typeof options == 'function' ) {
 		callback = options;
 		options = {};
 	}
 
+	// set the stopOnErrors option to default if no value specified
 	options.stopOnErrors = ( options.stopOnErrors === undefined ) ? true : options.stopOnErrors;
+
+	// invalid argument type
+	if ( typeof dir != 'string' || typeof callback != 'function' ) {
+		return callback( new Error( 'Invalid type of a path for Gerard specified.' ) );
+	}
+
+	// the given directory path contains patterns
+	if ( isPattern( dir ) ) {
+		readPattern( dir, options, callback );
+	} else {
+		readDir( dir, options, callback );
+	}
+}
+
+module.exports = gerard;
+
+function readPattern( dir, options, callback ) {
+	var parts = path.normalize( dir ).split( path.sep ),
+		results = [],
+		paths = [],
+		count = 0;
+
+	function checkDone( err, files ) {
+		count--;
+
+		if ( err && options.stopOnErrors ) {
+			return callback( err );
+		}
+
+		if ( !err ) {
+			results = results.concat( files );
+		}
+
+		if ( !count ) {
+			callback( null, results );
+		}
+	}
+
+	function readPart() {
+		var part = parts.shift();
+
+		if ( !part ) {
+			return callback( null, results );
+		}
+
+		console.log( 'read part', part );
+
+		if ( isPattern( part ) ) {
+			if ( part === '**' ) {
+				if ( parts.length ) {
+					// TODO globstar in the middle, what now? (-_-;)
+				} else {
+					count = paths.length;
+
+					console.log( paths );
+
+					paths.forEach( function( pth ) {
+						readDir( pth, checkDone );
+					} );
+				}
+			} else {
+				// wildcard in the middle of a path - update the paths list
+				if ( parts.length ) {
+					updatePaths( paths, part, function( result ) {
+						paths = result;
+						readPart();
+					} );
+					// wildcard at the end of a path
+				} else {
+					readAndFilterPaths( paths, part, function( result ) {
+						results = result;
+						readPart();
+					} );
+				}
+			}
+		} else {
+			// add current part to the paths
+			if ( parts.length ) {
+				if ( paths.length ) {
+					paths.forEach( function( pth, i ) {
+						paths[ i ] = path.join( pth, part );
+					} );
+				} else {
+					paths.push( part );
+				}
+			} else {
+
+			}
+
+			readPart();
+		}
+	}
+
+	readPart();
+
+	function updatePaths( paths, pattern, done ) {
+		var count = 0,
+			result = [];
+
+		function checkDone() {
+			count--;
+
+			if ( !count ) {
+				done( result );
+			}
+		}
+
+		paths.forEach( function( pth ) {
+			fs.readdir( pth, function( err, files ) {
+				if ( err && options.stopOnErrors ) {
+					return callback( err );
+				}
+
+				if ( err ) {
+					return;
+				}
+
+				count = files.length;
+
+				files.forEach( function( file ) {
+					var name = file;
+
+					file = path.join( pth, file );
+
+					if (
+						options.ignore &&
+						( typeof options.ignore == 'string' && minimatch( file, options.ignore ) ) ||
+						( options.ignore instanceof RegExp && options.ignore.test( file ) )
+					) {
+						return checkDone();
+					}
+
+					fs.stat( file, function( err, stats ) {
+						if ( err && options.stopOnErrors ) {
+							return callback( err );
+						}
+
+						if ( err ) {
+							return;
+						}
+
+						if ( stats.isDirectory() && minimatch( name, pattern ) ) {
+							result.push( file );
+						}
+
+						checkDone();
+					} );
+				} );
+			} );
+		} );
+	}
+
+	function readAndFilterPaths( paths, pattern, done ) {
+		var count = 0,
+			result = [];
+
+
+		function checkDone() {
+			count--;
+
+			if ( !count ) {
+				done( result );
+			}
+		}
+
+		paths.forEach( function( pth ) {
+			fs.readdir( pth, function( err, files ) {
+				if ( err && options.stopOnErrors ) {
+					return callback( err );
+				}
+
+				if ( err ) {
+					return;
+				}
+
+				count = files.length;
+
+				files.forEach( function( file ) {
+					var name = file;
+
+					file = path.join( pth, file );
+
+					if (
+						options.ignore &&
+						( typeof options.ignore == 'string' && minimatch( file, options.ignore ) ) ||
+						( options.ignore instanceof RegExp && options.ignore.test( file ) )
+					) {
+						return checkDone();
+					}
+
+					fs.stat( file, function( err, stats ) {
+						if ( err && options.stopOnErrors ) {
+							return callback( err );
+						}
+
+						if ( err ) {
+							return;
+						}
+
+						if ( minimatch( name, pattern ) ) {
+							result.push( file );
+						}
+
+						checkDone();
+					} );
+				} );
+			} );
+		} );
+	}
+}
+
+function readDir( dir, options, callback ) {
+	console.log( 'read dir', dir );
+	var results = [],
+		count = 0;
 
 	function decreaseCounter() {
 		count--;
@@ -55,11 +270,17 @@ function gerard( dir, options, callback ) {
 
 			file = path.join( dir, file );
 
-			if ( options.ignore && minimatch( file, options.ignore ) ) {
+			// apply ignore filter
+			if (
+				options.ignore &&
+				( typeof options.ignore == 'string' && minimatch( file, options.ignore ) ) ||
+				( options.ignore instanceof RegExp && options.ignore.test( file ) )
+			) {
+				console.log( 'ignore', file );
 				return decreaseCounter();
 			}
 
-			fs.lstat( file, function( err, stats ) {
+			fs.stat( file, function( err, stats ) {
 				if ( err && options.stopOnErrors ) {
 					return callback( err );
 				}
@@ -99,4 +320,7 @@ function gerard( dir, options, callback ) {
 	} );
 }
 
-module.exports = gerard;
+// check if the given string is a pattern
+function isPattern( str ) {
+	return str.indexOf( '*' ) > -1;
+}
--- test/test.js
+++ test/test.js
@@ -15,53 +15,90 @@ var expect = require( 'chai' ).expect,
 
 describe( 'Gerard', function() {
 	var dir1 = path.normalize( 'test/dir1' ),
+		testDir = path.normalize( 'test/' ),
 		invalid = path.normalize( 'invalid/path' );
 
-	it( 'should pass an error if invalid path was given', function( done ) {
-		gerard( invalid, function( err, results ) {
-			expect( err ).to.exist;
-			expect( results ).to.not.exist;
+	// it( 'should pass an error if invalid path was given', function( done ) {
+	// 	gerard( invalid, function( err, results ) {
+	// 		expect( err ).to.exist;
+	// 		expect( results ).to.not.exist;
+
+	// 		done();
+	// 	} );
+	// } );
+
+	// it( 'should list all the files in the directory', function( done ) {
+	// 	gerard( dir1, function( err, results ) {
+	// 		expect( err ).to.not.exist;
+	// 		expect( results ).to.be.an( 'array' );
+	// 		expect( results ).to.have.length( 10 );
+
+	// 		done();
+	// 	} );
+	// } );
+
+	// it( 'should handle "ignore" option', function( done ) {
+	// 	gerard( dir1, {
+	// 		ignore: '**/*.js'
+	// 	}, function( err, results ) {
+	// 		expect( err ).to.not.exist;
+	// 		expect( results ).to.be.an( 'array' );
+	// 		expect( results ).to.have.length( 2 );
+
+	// 		done();
+	// 	} );
+	// } );
+
+	// it( 'should handle "stats" option', function( done ) {
+	// 	gerard( dir1, {
+	// 		stats: true
+	// 	}, function( err, results ) {
+	// 		expect( err ).to.not.exist;
+	// 		expect( results ).to.be.an( 'array' );
+	// 		expect( results ).to.have.length( 10 );
+
+	// 		results.forEach( function( result ) {
+	// 			expect( result ).to.be.an( 'object' );
+	// 			expect( result ).to.contain.keys( [ 'name', 'dir', 'path', 'stats' ] );
+	// 		} );
+
+	// 		done();
+	// 	} );
+	// } );
+
+	it( 'test', function( done ) {
+		gerard( 'test/dir2', function( err, results ) {
+			if ( err ) {
+				console.error( 'err', err );
+			}
+
+			console.log( 'results', results.length, results );
 
 			done();
 		} );
 	} );
 
-	it( 'should list all the files in the directory', function( done ) {
-		gerard( dir1, function( err, results ) {
-			expect( err ).to.not.exist;
-			expect( results ).to.be.an( 'array' );
-			expect( results ).to.have.length( 4 );
+	it( 'should handle a pattern passed as a path ending with a globstar', function( done ) {
+		gerard( 'test/dir2/**', function( err, results ) {
+			if ( err ) {
+				console.error( 'err', err );
+			}
+
+			console.log( 'results', results.length, results );
 
 			done();
 		} );
 	} );
 
-	it( 'should handle "ignore" option', function( done ) {
-		gerard( dir1, {
-			ignore: '**/*.js'
-		}, function( err, results ) {
-			expect( err ).to.not.exist;
-			expect( results ).to.be.an( 'array' );
-			expect( results ).to.have.length( 0 );
+	// it( 'should handle a pattern passed as a path', function( done ) {
+	// 	gerard( 'test/dir2/*/*.js', function( err, results ) {
+	// 		if ( err ) {
+	// 			console.error( 'err', err );
+	// 		}
 
-			done();
-		} );
-	} );
+	// 		console.log( 'results', results );
 
-	it( 'should handle "stats" option', function( done ) {
-		gerard( dir1, {
-			stats: true
-		}, function( err, results ) {
-			expect( err ).to.not.exist;
-			expect( results ).to.be.an( 'array' );
-			expect( results ).to.have.length( 4 );
-
-			results.forEach( function( result ) {
-				expect( result ).to.be.an( 'object' );
-				expect( result ).to.contain.keys( [ 'name', 'dir', 'path', 'stats' ] );
-			} );
-
-			done();
-		} );
-	} );
+	// 		done();
+	// 	} );
+	// } );
 } );
